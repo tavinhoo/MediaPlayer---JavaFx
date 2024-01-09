16e299b659918033b13c61608f1144dad3359cf1
tavinhoo <luizotaviomtbs@gmail.com>
2024-01-09 13:02:52 -0300

    update

--- src/main/java/com/example/javafxmedia/MediaPlayerController.java
+++ src/main/java/com/example/javafxmedia/MediaPlayerController.java
@@ -2,13 +2,16 @@ package com.example.javafxmedia;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
 import java.io.File;
 
@@ -23,17 +26,34 @@ public class MediaPlayerController {
     @FXML
     private MediaView mediaView;
 
+    @FXML
+    private Slider slider;
+
+    private boolean isPlayed = false;
+
     private Media media;
     private MediaPlayer mediaPlayer;
 
     @FXML
     void btnPlay(MouseEvent event) {
 
+        if(!isPlayed) {
+            btnPlay.setText("Pause");
+            mediaPlayer.play();
+            isPlayed = true;
+        } else {
+            btnPlay.setText("Play");
+            mediaPlayer.pause();
+            isPlayed = false;
+        }
+
     }
 
     @FXML
     void btnStop(MouseEvent event) {
-
+        btnPlay.setText("Play");
+        mediaPlayer.stop();
+        isPlayed = false;
     }
 
     @FXML
@@ -56,8 +76,28 @@ public class MediaPlayerController {
 
             mediaView.setMediaPlayer(mediaPlayer);
 
-            mediaPlayer.setAutoPlay(true);
+            mediaPlayer.currentTimeProperty().addListener(((observableValue, oldValue, newValue) -> {
+                slider.setValue(newValue.toSeconds());
+                lblDuration.setText("Duration: " +(int) slider.getValue() + ":" + (int)media.getDuration().toSeconds());
+            }));
+
+            mediaPlayer.setOnReady(() -> {
+                Duration totalDuration = media.getDuration();
+                slider.setMax(totalDuration.toSeconds());
+                lblDuration.setText("Duration: 00" + ":" + totalDuration);
+            });
+
+            Scene scene = mediaView.getScene();
+            mediaView.fitWidthProperty().bind(scene.widthProperty());
+            mediaView.fitHeightProperty().bind(scene.heightProperty());
+
+            //mediaPlayer.setAutoPlay(true);
         }
+    }
+
+    @FXML
+    void sliderPressed(MouseEvent event) {
+        mediaPlayer.seek(Duration.seconds(slider.getValue()));
     }
 
 }
